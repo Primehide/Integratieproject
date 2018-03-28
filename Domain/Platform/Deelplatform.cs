ee0c4819a23bf2e62057af972cff3d7224d9ab60
primehide <primeservicespp@gmail.com>
2018-03-28 17:13:40 +0200

    Domain model for Platform + Relations

---
+++ Domain/Platform/Deelplatform.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Platform
+{
+    public class Deelplatform
+    {
+        public int DeelplatformId { get; set; }
+        public string Naam { get; set; }
+        public List<Domain.Entiteit.Entiteit> Entiteiten { get; set; }
+        public List<Pagina> Paginas { get; set; }
+    }
+}
