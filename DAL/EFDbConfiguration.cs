868780d70198cd53e0b77f0bfdf74f7251a6fbd3
primehide <primeservicespp@gmail.com>
2018-03-28 17:27:15 +0200

    EF migrations setup

--- DAL/EFDbConfiguration.cs
+++ DAL/EFDbConfiguration.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL
 {
-    class EFDbConfiguration
+    internal class EFDbConfiguration : DbMigrationsConfiguration<EFContext>
     {
+        public EFDbConfiguration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+        }
     }
 }
