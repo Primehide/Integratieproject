c83dba2c23107752bf20228419e1ae69dcda89e2
primehide <primeservicespp@gmail.com>
2018-03-28 18:27:38 +0200

    Managers, controllers and repositories for each package

---
+++ BL/Managers/AccountManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BL
+{
+    public class AccountManager
+    {
+        private IAccountRepository accountRepository;
+
+        public AccountManager()
+        {
+            accountRepository = new AccountRepository();
+        }
+    }
+}
---
+++ BL/Managers/PlatformManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BL
+{
+    public class PlatformManager
+    {
+        private IPlatformRepository platformRepository;
+
+        public PlatformManager()
+        {
+            platformRepository = new PlatformRepository();
+        }
+    }
+}
