d60f8244af720eccb2a6dc9fb4703e2c8bb78bba
WaffleDealer <Az015123>
2018-04-27 15:17:37 +0200

    test van api

---
+++ WebUI/Controllers/AndroidAPIController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebUI.Controllers
+{
+    public class AndroidAPIController : ApiController
+    {
+        private string eersteString { get; set; }
+        private string tweedeString { get; set; }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { eersteString, tweedeString };
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string een)
+        {
+            eersteString = een;
+        }
+
+    }
+}
