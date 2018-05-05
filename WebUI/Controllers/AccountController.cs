031b6faef0211a748c9f3ab2c013c080667f86ec
primehide <primeservicespp@gmail.com>
2018-05-05 11:12:16 +0200

    Correct view Confirm Email

--- WebUI/Controllers/AccountController.cs
+++ WebUI/Controllers/AccountController.cs
@@ -557,7 +557,7 @@ namespace WebUI.Controllers
         private async Task<string> SendEmailConfirmationTokenAsync(string userID, string subject)
         {
             string code = await UserManager.GenerateEmailConfirmationTokenAsync(userID);
-            var callbackUrl = Url.Action("ConfirmEmailNew", "Account",
+            var callbackUrl = Url.Action("ConfirmEmail", "Account",
                new { userId = userID, code = code }, protocol: Request.Url.Scheme);
             await UserManager.SendEmailAsync(userID, subject,
                "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
